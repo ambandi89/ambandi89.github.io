28bdb6fab2b5e9f50e8699ef80964294b90e085b
ambandi89 <ambandi89@gmail.com>
2025-09-03 09:54:05 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,43 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle("active");
   })
 })
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 300;
+});
+
+backBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 300;
+});
+
+// hover effect
+// new hoverEffect({
+// parent: document.querySelector(".distortion"),
+// intensity: 0.2,
+// image1: 'img/pexels-n-voitkevich-7077850.jpg',
+// image2: 'img/pexels-n-voitkevich-7077807.jpg',
+// displacementImage: 'img/heightMap.png',
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    new hoverEffect({
+      parent: document.querySelector(".distortion"),
+      intensity: 0.2,
+      image1: 'img/pexels-n-voitkevich-7077850.jpg',
+      image2: 'img/pexels-n-voitkevich-7077807.jpg',
+      displacementImage: 'img/heightMap.png',
+    });
+  });
